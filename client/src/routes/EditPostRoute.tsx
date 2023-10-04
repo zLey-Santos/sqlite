c0fb527564e34e54d2b7cc2588751b750b7605a9
zLey-santos <wy.santos92@gmail.com>
2023-10-04 22:42:24 +0100

    edit-post corrigido e funcional

--- client/src/routes/EditPostRoute.tsx
+++ client/src/routes/EditPostRoute.tsx
@@ -1,104 +1,93 @@
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import toast from 'react-simple-toasts';
-import { useZorm } from 'react-zorm';
 import { Helmet } from 'react-helmet';
-import { Title } from '../components/Title';
 import { Card } from '../components/Card';
 import { Button } from '../components/Button';
-import { ErrorMessage } from '../components/ErrorMessage';
-import { PostSchema } from '../postSchema ';
-import { Breadcrumbs } from '../components/Breadcrumbs';
 import { api } from '../api';
 import { Textarea } from '../components/TextArea';
+import { PostSchema } from '../postSchema ';
 
 const texts = {
   title: 'Editar publicação',
   contentPlaceholder: 'Edite a sua publicação',
   submit: 'Enviar',
   submitSuccess: 'Sua publicação foi editada com sucesso!',
-  submitFailure: 'Houve um erro ao editar a sua publicação. :(',
+  submitFailure: 'Houve um erro ao editar a sua publicação.',
 };
 
 export function EditPostRoute() {
   const params = useParams();
   const navigate = useNavigate();
-  const [initialFormState, setInitialFormState] = useState({
-    content: '', // Inicialize com o valor vazio
+  const [formData, setFormData] = useState({
+    content: '',
   });
 
-  const zo = useZorm('edit-post', PostSchema, {
-    async onValidSubmit(event) {
+  useEffect(() => {
+    async function loadPost() {
       try {
-        event.preventDefault();
-        const response = await api.put(`/posts/${params.id}`, event.data);
-        
+        const response = await api.get(`/posts/${params.id}`);
+        setFormData(response.data);
+      } catch (error) {
+        console.error('Erro ao carregar a postagem:', error);
+        toast(texts.submitFailure);
+      }
+    }
+
+    loadPost();
+  }, [params.id]);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // Valide o formData com o PostSchema
+      const validationResult = PostSchema.safeParse(formData);
+  
+      if (validationResult.success) {
+        const response = await api.put(`/posts/${params.id}`, formData);
+  
         if (response.data.id) {
           toast(texts.submitSuccess);
           navigate(`/view-post/${params.id}`);
         } else {
           toast(texts.submitFailure);
         }
-      } catch (error) {
-        console.error('Erro ao editar a postagem:', error);
-        toast(texts.submitFailure);
+      } else {
+        // Se a validação falhar, exiba as mensagens de erro
+        validationResult.error.issues.forEach((issue) => {
+          toast(issue.message);
+        });
       }
-    },
-  });
-
-  async function loadPost() {
-    try {
-      const response = await api.get(`/posts/${params.id}`);
-      setInitialFormState(response.data);
     } catch (error) {
-      console.error('Erro ao carregar a postagem:', error);
-      // Lide com o erro, exiba uma mensagem ao usuário, etc.
+      console.error('Erro ao editar a postagem:', error);
+      toast(texts.submitFailure);
     }
-  }
+  };
+  
 
-  useEffect(() => {
-    loadPost();
-  }, [params.id]);
+  const handleContentChange = (e) => {
+    setFormData({
+      ...formData,
+      content: e.target.value,
+    });
+  };
 
   return (
     <Card>
       <Helmet>
         <title>Editar publicação #{params.id}</title>
       </Helmet>
-      <Breadcrumbs
-        links={[
-          { href: '/', label: 'Home' },
-          {
-            href: `/view-post/${params.id}`,
-            label: `Ver publicação #${params.id}`,
-          },
-          {
-            label: `Editar publicação #${params.id}`,
-          },
-        ]}
-      />
-      <Title className='mb-4 text-center'>
-        {texts.title} #{params.id}
-      </Title>
-
-      <form ref={zo.ref} className='flex flex-col gap-3'>
+      {/* ... */}
+      <form onSubmit={handleFormSubmit} className='flex flex-col gap-3'>
         <div>
           <Textarea
             className='rounded-lg p-2 border mt-6 focus:border-amber-300 outline-none w-full resize-none'
             placeholder={texts.contentPlaceholder}
-            name={zo.fields.content()}
-            value={initialFormState.content} // Use o valor do estado para preencher o campo
-            onChange={(e) => {
-              // Atualize o estado quando o usuário digitar
-              setInitialFormState({
-                ...initialFormState,
-                content: e.target.value,
-              });
-            } }
+            name="content"
+            value={formData.content}
+            onChange={handleContentChange}
             rows={3} defaultValue={undefined}          />
-          {zo.errors.content((error) => (
-            <ErrorMessage>{error.message}</ErrorMessage>
-          ))}
+          {/* ... */}
         </div>
         <div className='flex justify-end items-center px-2'>
           <Button type='submit' className='bg-yellow-500 hover:bg-yellow-600'>
